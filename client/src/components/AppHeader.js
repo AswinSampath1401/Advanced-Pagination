05b9cb2f121488dd964cc1c47756b1c2624c9416
Aswin Sampath <aswinsampath1401@gmail.com>
2021-01-31 16:16:09 +0530

    💲 Front End Designing done

---
+++ client/src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AppHeader.css';
+
+const AppHeader = () => {
+    return (
+        <div className='app_header'>
+            Pagination Using React
+        </div>
+    )
+}
+
+export default AppHeader
---
+++ client/src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+const Card = ({post}) => {
+    return (
+        <div className='card'>
+            <p className='card_title'>{post.title}</p>
+            <p className='card_author'>{post.author}</p>
+            <p className='card_body'>{post.body}</p>
+        </div>
+    )
+}
+
+export default Card;
